ad64c2de9fd96810960846b0209ed9fc6da658e7
ZanAmbrozic <zan.ambrozic@gmail.com>
2023-02-13 21:42:34 +0100

    Player selection is now functional
    User can now select character from menu screen

--- Akimbo Stickman/Assets/Stickman/CameraSystem.cs
+++ Akimbo Stickman/Assets/Stickman/CameraSystem.cs
@@ -9,18 +9,21 @@ public class CameraSystem : MonoBehaviour
     public float xMax;
     public float yMin;
     public float yMax;
-    public bool enableCamera = true;
+    public bool enableCameraMovement = true;
 
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
-        enabled = enableCamera;
+        enabled = enableCameraMovement;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        if (_player == null)
+            _player = GameObject.FindGameObjectWithTag("Player");
+
         float x = Mathf.Clamp(_player.transform.position.x, xMin, xMax);
         float y = Mathf.Clamp(_player.transform.position.y, yMin, yMax);
         gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
